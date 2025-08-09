2e46008b0038af77aa96aaf9c22078fbde656903
Lucas Campos <bhlucascampos@gmail.com>
2025-08-09 10:14:59 -0300

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
+import { Home } from './home/home';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+import { Dashboard } from './dashboard/dashboard';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
     children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' }
+      { path: 'login', component: Login },
+      { path: 'register', component: Register },
+      { path: '', redirectTo: 'login', pathMatch: 'full' }
     ]
   },
+  { path: 'dashboard', component: Dashboard },
   { path: '**', redirectTo: '' }
 ];
--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      cpfOrEmail: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { cpfOrEmail, password } = this.loginForm.value;
+      if (cpfOrEmail === 'teste@teste.com' && password === '123') {
+        this.router.navigate(['/dashboard']);
+      } else {
+        // Lógica para credenciais inválidas (ex: exibir mensagem de erro)
+        console.error('Credenciais inválidas');
+      }
+    }
+  }
 }
